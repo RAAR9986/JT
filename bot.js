8460accdb9f7b08d5ce1822ef5fdc183f4e5e866
RAAR9986 <50454857+RAAR9986@users.noreply.github.com>
2019-05-21 06:06:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -453,7 +453,40 @@ client.on('message', function(msg) {
     }
   });
 
-
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'mute')){
+        let role = message.guild.roles.find(r => r.name === 'Muted');
+        if(!role) message.guild.createRole({name: 'Muted'});
+         if(user.bot){
+            return message.channel.send(`I can't mute ${user} because he is a bot`);
+      }
+      if(user.hasPermission('ADMINISTRATOR')) {
+          return message.channel.send(`I can't mute ${user} because he is staff`);
+        }
+       
+        if(!user){
+            message.channel.send(`There's no person to mute tho`);
+      }
+      message.guild.channels.forEach(f => {
+          f.overwritePermissions(role, {
+              SEND_MESSAGES: false
+          });
+          user.addRole(role);
+       
+      });
+       message.channel.send(`I muted ${user}`);
+  }
+  });
+   client.on('message', message => {
+   if(message.content.startsWith(prefix + 'unmute')){
+       let role = message.guild.roles.find(r => r.name === 'Muted');
+   if(!user.roles.has(role)) {
+       return message.channel.send(`He is not muted`);
+   }
+       user.removeRole(role).then(message.channel.send(`Unmuted ${user}`));
+     
+   }
+   });
 
 
 
